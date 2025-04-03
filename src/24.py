e65d0ab111411cb8732422e08b13a4117a5e217c
rahmanakko164 <rahmanakko164@gmail.com>
2025-04-03 19:04:35 +0000

    Add 24.py

---
+++ src/24.py
@@ -0,0 +1,9 @@
+import math
+
+def calculate_area(radius):
+    area = math.pi * radius ** 2
+    return area
+
+radius = float(input("Enter the radius: "))
+area = calculate_area(radius)
+print(f"The area of the circle with radius {radius} is {area:.2f}")
